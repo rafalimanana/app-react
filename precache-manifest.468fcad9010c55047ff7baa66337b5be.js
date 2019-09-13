6532a4fbc73662c763478aba5580ae82d6f990ec
Fabrice <rafalimananafabrice@gmail.com>
2019-09-13 12:04:53 +0300

    Updates

---
+++ precache-manifest.468fcad9010c55047ff7baa66337b5be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6967ca90db86685869d9d9c0e76d78cd",
+    "url": "/rafalimanana/app-react/index.html"
+  },
+  {
+    "revision": "43bc29cf4fe13cfe60ed",
+    "url": "/rafalimanana/app-react/static/css/main.f7e1efb5.chunk.css"
+  },
+  {
+    "revision": "3c21542418b1eb15dd20",
+    "url": "/rafalimanana/app-react/static/js/2.0931e701.chunk.js"
+  },
+  {
+    "revision": "43bc29cf4fe13cfe60ed",
+    "url": "/rafalimanana/app-react/static/js/main.8e05d0f9.chunk.js"
+  },
+  {
+    "revision": "0b0508bb1a444d0b95d5",
+    "url": "/rafalimanana/app-react/static/js/runtime~main.378d44be.js"
+  }
+]);
